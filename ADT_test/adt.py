af06d976884756a53ae0224e30cad11b75603718
EntityFile <47143799+EntityFile@users.noreply.github.com>
2019-04-22 22:12:05 +0300

    Update adt.py

--- ADT_test/adt.py
+++ ADT_test/adt.py
@@ -3,12 +3,6 @@ from urllib.parse import quote, urlsplit, urlunsplit
 import json
 
 
-def input_track_name():
-    name = input('Type track name title: ').split()
-    name = ' '.join(name)
-    return name
-
-
 class PlayList:
     def __init__(self):
         """
@@ -159,51 +153,6 @@ def create_lyrics(author, name):
         print('Error, wrong track titles')
 
 
-class YouTube:
-    def __init__(self, artist, track):
-        self.artist = artist
-        self.track = track
-        self.url = None
-        self.title = None
-
-    def get_info(self):
-        url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=' + '%20'.join(
-            self.artist.split()) + '%20' + '%20'.join(self.track.split()) \
-              + '&key=AIzaSyC3kzqz8N9kVTAKLXHUl2ZZTi6aYCwMZM0'
-        print(url)
-        print(self.artist)
-        print(self.track)
-        content = urllib.request.urlopen(url)
-        f = json.load(content)
-        i = 0
-        try:
-            while f['items'][i]['id']['kind'] != 'youtube#video':
-                i += 1
-                if i > 25:
-                    break
-            self.title = f['items'][i]['snippet']['title']
-            self.url = 'https://www.youtube.com/watch?v=' + f['items'][i]['id']['videoId']
-        except:
-            print('Wrong artist/track name!', f['items'])
-
-
-class Itunes:
-    def __init__(self, inpt):
-        self.songs = []
-        self.inpt = inpt
-        self.ids = 0
-
-    def get_songs(self):
-        content = urllib.request.urlopen('https://itunes.apple.com/search?term=' + self.inpt)
-        f = json.load(content)
-        self.songs = f['results']
-        self.ids = len(self.songs)
-
-    def return_songs(self):
-        self.get_songs()
-        return self.songs
-
-
 class Song:
     def __init__(self):
         self.artist_id = None
@@ -283,55 +232,4 @@ class Song:
 
     def __str__(self):
         return self.artist + ' : ' + self.track_name
-
-
-def maiin():
-    name = input_track_name()
-    name = iri_conventer(name)
-    print(name)
-    itunes_info = Itunes(name)
-    title_list = []
-    songs = itunes_info.return_songs()
-    print(songs)
-    for i in range(itunes_info.ids):
-        if songs[i]['wrapperType'] == 'track':
-            song = Song()
-            song.get_data(songs, i)
-            if not (song.artist, song.track_name) in title_list:
-                youtube = YouTube(song.artist, song.track_name)
-                #	youtube.get_info()
-                #	video = youtube.url
-                #	title = youtube.title
-                #	print(title)
-                #song.get_lyrics()
-                print(song.track_name, song.artist, song.track_url, song.track_id)
-                print()
-                print(song.lyrics)
-                #	print(video)
-                print()
-                title_list.append((song.artist, song.track_name))
-
-
-def create_output_json_file(lst):
-    f = open('output.json', 'w', encoding='utf-8')
-    data_dict = dict()
-    for el in lst:
-        data_dict[el[0]] = el[1]
-    json.dump(data_dict, f)
-    f.close()
-
-
-def iri_conventer(text):
-    parts = urlsplit(text)
-    url_code = urlunsplit((
-        parts.scheme,
-        parts.netloc.encode('idna').decode('ascii'),
-        quote(parts.path),
-        quote(parts.query, '='),
-        quote(parts.fragment),
-    ))
-    return url_code
-
-
-if __name__ == '__main__':
-    maiin()
+    
